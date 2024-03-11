fb8dd617269a33c632fd2dc8479c5fa5b1e1e44c
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-03-11 09:04:21 -0400

    serverUpdatePg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
+if (process.NODE_ENV != 'production') { require('dotenv').config(); }
 const cors = require('cors')
-if (process.env.NODE != 'production') require('dotenv').config()
 
 const app = express()
 
