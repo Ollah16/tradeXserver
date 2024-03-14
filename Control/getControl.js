facd64725192f5ea381b888dd42d034639c2072e
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-03-14 13:15:14 -0400

    tradexUpdate

--- Control/getControl.js
+++ Control/getControl.js
@@ -1,15 +1,14 @@
 const { default: axios } = require("axios");
 
-exports.handleFetchRate = async (req, res) => {
+exports.handleFetchRate = (req, res) => {
 
-    await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest ', {
+    axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest ', {
         headers: {
             'X-CMC_PRO_API_KEY': `${process.env.COINMARKET}`,
             'Content-Type': 'application/x-www-form-urlencoded'
         },
     })
         .then((response) => {
-            console.log(data)
             const { data } = response.data
             res.json(data)
         })
