3459bb1244a5dd3e735282e0e0cd7eaff1b2c3a8
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-03-14 08:53:54 -0400

    crypto

--- Control/getControl.js
+++ Control/getControl.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 exports.handleFetchRate = async (req, res) => {
     console.log('helllo')
 
-    await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map ', {
+    await axios.get('https://pro-api.coinmarketcap.com/cryptocurrency ', {
         headers: {
             'X-CMC_PRO_API_KEY': `${process.env.COINMARKET}`,
             'Content-Type': 'application/x-www-form-urlencoded'
